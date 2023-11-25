1fd9e70c865add21edacb930470e139c22395e28
Nele Hirsch <nele@ebildungslabor.de>
2023-11-25 17:16:59 +0100

    Update generate.sh
    Nummer wird hier generiert und muss nicht mehr aus der URL gelesen werden.

--- generate.sh
+++ generate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Generiert die HTML Dateien basierend auf der cards.template Datei.
-# Die Aussagen werden aus der cards.lst Datei gelesen und in die HTML Dateien eingefügt.
+# Generiert die HTML-Dateien basierend auf der cards.template-Datei und cards.lst-Datei.
+# Die Aussagen werden aus der cards.lst-Datei gelesen und in die HTML-Dateien eingefügt.
+# Es wird die Nummer generiert und eingefügt.
 
 count=0
 while read -r line
 do
     count=$(( count + 1 ))
-    sed -e "s/##HTML##/$line/g" cards.template > $count.html
+    sed -e "s|<!-- Inhalt der Karte -->|$line|g; s|<!-- Kartennummer -->|$count|g" cards.template > "$count.html"
 done < cards.lst
